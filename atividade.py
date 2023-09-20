ee9217d5a67766d2e4cc4917c9259c9d9c3013f2
lucasvna <132510177+lucasvna@users.noreply.github.com>
2023-09-20 19:22:08 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def somar(a, b):
+    return a + b
+
+
+class TestSoma(unittest.TestCase):
+
+    def test_soma_positivos(self):
+        self.assertEqual(somar(2, 3), 5)
+
+    def test_soma_negativos(self):
+        self.assertEqual(somar(-1, -1), -2)
+
+
+if __name__ == '__main__':
+    unittest.main()
